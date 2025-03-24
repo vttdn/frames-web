6b5a5fa3dff3bb2c7787d7088b71a3337af23ae8
Vincent Tantardini <v@vnn.io>
2025-03-24 19:16:58 +0700

    refinements

--- js/googleAnalytics.js
+++ js/googleAnalytics.js
@@ -1,7 +1,1 @@
- window.dataLayer = window.dataLayer || [];
-      function gtag() {
-        dataLayer.push(arguments);
-      }
-      gtag("js", new Date());
-
-      gtag("config", "G-MER5TLYKLF");
+function gtag(){dataLayer.push(arguments)}window.dataLayer=window.dataLayer||[],gtag("js",new Date),gtag("config","G-MER5TLYKLF");
