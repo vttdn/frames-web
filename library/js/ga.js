849a597f2ca647cc62ecb31e8f4bed12dd8ef45f
Vincent Tantardini <v@vnn.io>
2025-05-18 21:01:46 +0700

    ga fully external js

---
+++ library/js/ga.js
@@ -0,0 +1,4 @@
+function gtag() {
+    dataLayer.push(arguments);
+}
+(window.dataLayer = window.dataLayer || []), gtag("js", new Date()), gtag("config", "G-MER5TLYKLF", { transport_type: "beacon" });
